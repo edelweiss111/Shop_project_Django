63651a548f0ee869db120fa9a6812c1ee8588873
Saveliy <ya.savchik2000@mail.ru>
2023-10-29 16:10:29 +0400

    DZ 4 add user product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,4 @@
-from catalog.views import home_page, contact, product
+from catalog.views import home_page, contacts, products, user_product
 from django.urls import path
 from catalog.apps import CatalogConfig
 
@@ -6,6 +6,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('contacts/', contact, name='contact'),
-    path('products/', product, name='product')
+    path('contacts/', contacts, name='contacts'),
+    path('products/', products, name='products'),
+    path('add/', user_product, name='user_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from catalog.models import Product, Contact
+from catalog.models import Product, Contact, Category
 
 
 # Create your views here.
 def home_page(request):
     """Контроллер домашней страницы"""
-    products = Product.objects.all()
+    products_list = Product.objects.all()
     last_products = []
-    for item in reversed(products):
+    for item in reversed(products_list):
         last_products.append(item)
     context = {
         'product_list': last_products[:5],
@@ -16,11 +16,11 @@ def home_page(request):
     return render(request, 'catalog/home_page.html', context=context)
 
 
-def contact(request):
+def contacts(request):
     """Контроллер страницы контактов"""
-    contacts = Contact.objects.all()
+    contacts_list = Contact.objects.all()
     data = {
-        'contact_list': contacts,
+        'contact_list': contacts_list,
     }
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -30,11 +30,28 @@ def contact(request):
     return render(request, 'catalog/contact.html', context=data)
 
 
-def product(request):
+def products(request):
     """Контроллер страницы товаров"""
-    products = Product.objects.all()
+    products_list = Product.objects.all()
     context = {
-        'product_list': products,
+        'product_list': products_list,
     }
 
-    return render(request, 'catalog/product.html', context=context)
+    return render(request, 'catalog/products.html', context=context)
+
+
+def user_product(request):
+    """Контроллер страницы товаров"""
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list,
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        image = 'products/' + request.POST.get('image')
+        price = request.POST.get('price')
+        category = Category.objects.get(name=request.POST.get('category'))
+        print(f'1 - {name}, 2 - {description}, 3 - {image}, 4 - {price}, 5 - {category}')
+        Product.objects.create(name=name, description=description, image=image, price=price, category=category)
+    return render(request, 'catalog/user_product.html', context=context)
